a99eaa62fb15d46c41dad175995a846fd7cd925c
Palawi <bapcoloj@gmail.com>
2026-01-03 19:46:29 +0100

    salut le s

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-    plugins: [vue(), tailwindcss()],
-    base: '/portfolio/',
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets'
+export default defineConfig(({ command }) => {
+    const base = command === 'serve' ? '/' : '/portfolio/'
+
+    return {
+        plugins: [vue(), tailwindcss()],
+        base,
+        build: {
+            outDir: 'dist',
+            assetsDir: 'assets'
+        }
     }
 })
